126dc5b4f786e2a9c82481fb9a6afffc87ec8972
Paul DeCarlo <pdecarlo@microsoft.com>
2018-08-17 21:22:51 -0500

    Update to support Edge GA runtime

--- Dockerfile
+++ Dockerfile
@@ -7,22 +7,30 @@ RUN apt-get update -qq && apt-get install -qqy \
     wget \
     gnupg \
     lsb-release \
-    jq
+    jq \
+    net-tools \
+    iproute2 \
+    systemd && \
+    rm -rf /var/lib/apt/lists/*
 
 RUN AZ_REPO=$(lsb_release -cs) && \
     echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | \
     tee /etc/apt/sources.list.d/azure-cli.list && \
     curl -L https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
 
-RUN apt-get update -qq && apt-get install -qqy \
-    python-pip \
-    azure-cli
+RUN curl https://packages.microsoft.com/config/ubuntu/18.04/prod.list > ./microsoft-prod.list && \
+    cp ./microsoft-prod.list /etc/apt/sources.list.d/ && \
+    curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg && \
+    cp ./microsoft.gpg /etc/apt/trusted.gpg.d/ 
 
+RUN apt-get update && apt-get install -y --no-install-recommends \
+    azure-cli \
+    moby-cli \
+    moby-engine && \ 
+    apt-get install -y --no-install-recommends iotedge && \ 
+    rm -rf /var/lib/apt/lists/*
+    
 RUN az extension add --name azure-cli-iot-ext
-
-RUN pip install -U azure-iot-edge-runtime-ctl
-
-RUN curl -sSL https://get.docker.com/ | sh
 
 COPY edge-provision.sh /usr/local/bin/
 RUN chmod +x /usr/local/bin/edge-provision.sh
@@ -30,6 +38,8 @@ RUN chmod +x /usr/local/bin/edge-provision.sh
 VOLUME /var/lib/docker
 
 EXPOSE 2375
+EXPOSE 15580
+EXPOSE 15581
 
 ENTRYPOINT ["bash", "edge-provision.sh"]
 
--- edge-provision.sh
+++ edge-provision.sh
@@ -1,23 +1,48 @@
-    #!/bin/bash
+#!/bin/bash
 
-    provision(){
-    echo "***Start Docker in Docker***"
-    dockerd --host=unix:///var/run/docker.sock --host=tcp://0.0.0.0:2375 &
-    echo "***Authenticating with Azure CLI***"
-    az login --service-principal -u $spAppUrl -p $spPassword --tenant $tenantId
-    az account set --subscription $subscriptionId
-    echo "***Configuring IoT Edge Runtime***"
-    az iot hub device-identity create --device-id $(hostname) --hub-name $iothub_name --edge-enabled
-    connectionString=$(az iot hub device-identity show-connection-string --device-id $(hostname) --hub-name $iothub_name | jq -r '.cs')
-    az iot hub device-twin update --device-id $(hostname) --hub-name $iothub_name --set tags='{"environment":"'$environment'"}'
+provision(){
+echo "***Start Docker in Docker***"
+dockerd --host=unix:///var/run/docker.sock --host=tcp://0.0.0.0:2375 &
+echo "***Authenticating with Azure CLI***"
+az login --service-principal -u $spAppUrl -p $spPassword --tenant $tenantId
+az account set --subscription $subscriptionId
+echo "***Configuring IoT Edge Device***"
+az iot hub device-identity create --device-id $(hostname) --hub-name $iothub_name --edge-enabled
+connectionString=$(az iot hub device-identity show-connection-string --device-id $(hostname) --hub-name $iothub_name | jq -r '.cs')
+az iot hub device-twin update --device-id $(hostname) --hub-name $iothub_name --set tags='{"environment":"'$environment'"}'
+echo "***Configuring and Starting IoT Edge Runtime***"
 
-    echo "***The connection string***"
-    docker info
+IP=$(ifconfig eth0 | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
 
-    iotedgectl setup --connection-string $connectionString --auto-cert-gen-force-no-passwords
-    iotedgectl login --address $acr_host --username $acr_user --password $acr_password
-    iotedgectl start
-    }
+cat <<EOF > /etc/iotedge/config.yaml
+provisioning:
+  source: "manual"
+  device_connection_string: "$connectionString"
+agent:
+  name: "edgeAgent"
+  type: "docker"
+  env: {}
+  config:
+    image: "mcr.microsoft.com/azureiotedge-agent:1.0"
+    auth: {}
+hostname: $(cat /proc/sys/kernel/hostname)
+connect:
+  management_uri: "http://$IP:15580"
+  workload_uri: "http://$IP:15581"
+listen:
+  management_uri: "http://$IP:15580"
+  workload_uri: "http://$IP:15581"
+homedir: "/var/lib/iotedge"
+moby_runtime:
+  docker_uri: "/var/run/docker.sock"
+  network: "azure-iot-edge"
+EOF
 
-    # Check Arguments
-    provision
+cat /etc/iotedge/config.yaml
+
+iotedged -c /etc/iotedge/config.yaml
+
+}
+
+# Check Arguments
+provision
