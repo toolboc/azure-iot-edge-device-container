ee71f4556425fb1bcb2d445e42a7adc50365dd69
Paul DeCarlo <pdecarlo@microsoft.com>
2018-09-17 16:38:17 -0500

    Add support for presupplied connectionString

--- edge-provision.sh
+++ edge-provision.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 provision(){
-echo "***Start Docker in Docker***"
-dockerd --host=unix:///var/run/docker.sock --host=tcp://0.0.0.0:2375 &
 echo "***Authenticating with Azure CLI***"
 az login --service-principal -u $spAppUrl -p $spPassword --tenant $tenantId
 az account set --subscription $subscriptionId
@@ -10,6 +8,9 @@ echo "***Configuring IoT Edge Device***"
 az iot hub device-identity create --device-id $(hostname) --hub-name $iothub_name --edge-enabled
 connectionString=$(az iot hub device-identity show-connection-string --device-id $(hostname) --hub-name $iothub_name | jq -r '.cs')
 az iot hub device-twin update --device-id $(hostname) --hub-name $iothub_name --set tags='{"environment":"'$environment'"}'
+}
+
+startEdgeRuntime(){
 echo "***Configuring and Starting IoT Edge Runtime***"
 
 IP=$(ifconfig eth0 | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
@@ -44,5 +45,11 @@ iotedged -c /etc/iotedge/config.yaml
 
 }
 
-# Check Arguments
-provision
+echo "***Start Docker in Docker***"
+dockerd --host=unix:///var/run/docker.sock --host=tcp://0.0.0.0:2375 &
+
+if [ -z "$connectionString" ]; then
+    echo "No connectionString provided, provisioning as a brand new IoTEdge device with name: $(hostname)"
+    provision
+fi 
+startEdgeRuntime
